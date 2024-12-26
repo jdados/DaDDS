eea0e325eee35fde0806ed7aac88133c18a5c49f
jdados <jeremiasdados@ufl.edu>
2024-12-26 11:59:19 -0500

    fixed top level name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module ook_dds_top_level (
+module tt_um_ook_dds (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
